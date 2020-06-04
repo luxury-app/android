e255b107b6e2b69ba4bc9d70b95c564fde611f63
zshel <sheltonz@wit.edu>
2020-06-04 09:21:09 -0400

    Initial project push

---
+++ app/src/main/java/nz/shleto/luxury/MainActivity.java
@@ -0,0 +1,14 @@
+package nz.shleto.luxury;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
